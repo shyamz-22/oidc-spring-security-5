dd63fa8e581545de75bcde402a3162ec42eb17ad
Syamala Umamaheswaran <shykart2203@gmail.com>
2019-03-24 16:35:33 +0100

    Do not test HelloController with mocking Security Context

--- src/main/kotlin/com/github/shyamz22/oidcspringsecurity5/HelloController.kt
+++ src/main/kotlin/com/github/shyamz22/oidcspringsecurity5/HelloController.kt
@@ -3,6 +3,7 @@ package com.github.shyamz22.oidcspringsecurity5
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,8 +15,9 @@ class HelloController {
     }
 
     @GetMapping("/me")
-    fun hello(): ResponseEntity<String> {
-        return ResponseEntity.ok("ok")
+    fun hello(currentUser: OAuth2AuthenticationToken): ResponseEntity<OAuth2AuthenticationToken> {
+        log.warn("User authentication successful")
+        return ResponseEntity.ok(currentUser)
     }
 
 }
