ed8bdd16961eadf3b533c366c7e148c32a049c79
Syamala Umamaheswaran <shykart2203@gmail.com>
2019-03-24 16:27:24 +0100

    Add failing test for HelloController

--- src/main/kotlin/com/github/shyamz22/oidcspringsecurity5/HelloController.kt
+++ src/main/kotlin/com/github/shyamz22/oidcspringsecurity5/HelloController.kt
@@ -1,4 +1,21 @@
 package com.github.shyamz22.oidcspringsecurity5
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HelloController {
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(HelloController::class.java)
+    }
+
+    @GetMapping("/me")
+    fun hello(): ResponseEntity<String> {
+        return ResponseEntity.ok("ok")
+    }
+
 }
