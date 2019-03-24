018f0717bba7f3d803b9b6f6787c1b9472eed42c
Syamala Umamaheswaran <shykart2203@gmail.com>
2019-03-24 13:52:36 +0100

    Initial setup

---
+++ src/main/kotlin/com/github/shyamz22/oidcspringsecurity5/OidcSpringSecurity5Application.kt
@@ -0,0 +1,11 @@
+package com.github.shyamz22.oidcspringsecurity5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OidcSpringSecurity5Application
+
+fun main(args: Array<String>) {
+    runApplication<OidcSpringSecurity5Application>(*args)
+}
---
+++ src/test/kotlin/com/github/shyamz22/oidcspringsecurity5/OidcSpringSecurity5ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.github.shyamz22.oidcspringsecurity5
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class OidcSpringSecurity5ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
